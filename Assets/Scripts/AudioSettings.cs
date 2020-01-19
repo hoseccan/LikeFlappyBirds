060727cc790d41a56ec333447ad4df358183f1d2
hoseccan <hoseccan@gmail.com>
2020-01-19 18:48:04 +0700

    First commit

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    public int            state = 1;
+
+    public GameObject     activeIcon;
+    public GameObject     deactiveIcon;
+
+    public void Awake()
+    {
+        LoadState();
+    }
+
+    public void OnApplicationQuit()
+    {
+        SaveState();
+    }
+
+    public void OnApplicationPause(bool pause)
+    {
+        SaveState();
+    }
+
+    public void Switch()
+    {
+        if(state == 1)
+        {
+            activeIcon.SetActive(false);
+            deactiveIcon.SetActive(true);
+
+            state = 0;
+            AudioListener.volume = state;
+        }
+        else
+        {
+            activeIcon.SetActive(true);
+            deactiveIcon.SetActive(false);
+
+            state = 1;
+            AudioListener.volume = state;
+        }
+    }
+
+    public void LoadState()
+    {
+        state = PlayerPrefs.GetInt("listener");
+        AudioListener.volume = state;
+
+        if (state > 0)
+        {
+            activeIcon.SetActive(true);
+            deactiveIcon.SetActive(false);
+        }
+        else
+        {
+            activeIcon.SetActive(false);
+            deactiveIcon.SetActive(true);
+        }
+    }
+
+    public void SaveState()
+    {
+        PlayerPrefs.SetInt("listener",state);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using System;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D       playerRigidbody;
+    private GameObject        playerModel;
+    private CapsuleCollider2D playerCollider;
+    public  Singleton         singleton;
+    private AudioSource       audio;
+
+    public  float             forseOfJump;
+    private Vector3           previousPos;
+    private Vector3           currentPos;
+    private Vector3           vectorOfSpeed;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerModel     = GetComponent<Transform>().GetChild(0).gameObject;
+        playerCollider  = GetComponent<CapsuleCollider2D>();
+        audio           = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Jump();
+        RotateModel();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            playerRigidbody.AddForce(new Vector2(0, forseOfJump) * Time.deltaTime * 10000f);
+            audio.Play();
+        }
+    }
+
+    private void RotateModel()
+    {
+        Vector2 velocity = playerRigidbody.velocity;
+
+        if ((velocity.y > 0) && (playerModel.transform.rotation.z > -30f) && (playerModel.transform.rotation.z <= 0))
+        {
+            playerModel.transform.Rotate(new Vector3(0, 0, 25f));
+        }
+        if ((velocity.y < 0) && (playerModel.transform.rotation.z < 30f) && (playerModel.transform.rotation.z >= 0))
+        {
+            playerModel.transform.Rotate(new Vector3(0, 0, -25f));
+        }
+    }
+
+    public void OnCollisionEnter2D()
+    {
+        singleton.GameOver();
+    }
+}
